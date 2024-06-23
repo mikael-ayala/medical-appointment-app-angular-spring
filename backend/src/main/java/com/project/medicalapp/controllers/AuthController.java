33c6b779f8d5b290eac3a148bd16b22e5f9ceb0d
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-06-23 11:42:51 -0300

    refactoring authController and adding authService

--- backend/src/main/java/com/project/medicalapp/controllers/AuthController.java
+++ backend/src/main/java/com/project/medicalapp/controllers/AuthController.java
@@ -3,77 +3,47 @@ package com.project.medicalapp.controllers;
 import com.project.medicalapp.dto.LoginRequestDTO;
 import com.project.medicalapp.dto.RegisterRequestDTO;
 import com.project.medicalapp.dto.ResponseDTO;
-import com.project.medicalapp.entities.Role;
-import com.project.medicalapp.entities.User;
-import com.project.medicalapp.infra.security.TokenService;
-import com.project.medicalapp.repositories.UserRepository;
+import com.project.medicalapp.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final TokenService tokenService;
+    private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
-        User user = this.userRepository.findByEmail(body.email())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO body) {
+        ResponseDTO response = authService.login(body);
 
-        if (passwordEncoder.matches(body.password(), user.getPassword())) {
-            String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        if (response == null) {
+            return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/patient-register")
-    public ResponseEntity patientRegister(@RequestBody RegisterRequestDTO body) {
-        Optional<User> user = this.userRepository.findByEmail(body.email());
-        if (user.isEmpty()) {
-            User newUser = new User();
+    public ResponseEntity<ResponseDTO> patientRegister(@RequestBody RegisterRequestDTO body) {
+        ResponseDTO response = authService.patientRegister(body);
 
-            newUser.setPassword(passwordEncoder.encode(body.password()));
-            newUser.setEmail(body.email());
-            newUser.setName(body.name());
-            newUser.setLastname(body.lastname());
-            newUser.setRole(Role.ROLE_PATIENT);
-
-            this.userRepository.save(newUser);
-
-            String token = this.tokenService.generateToken(newUser);
-            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        if (response == null) {
+            return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/doctor-register")
-    public ResponseEntity doctorRegister(@RequestBody RegisterRequestDTO body) {
-        Optional<User> user = this.userRepository.findByEmail(body.email());
-        if (user.isEmpty()) {
-            User newUser = new User();
+    public ResponseEntity<ResponseDTO> doctorRegister(@RequestBody RegisterRequestDTO body) {
+        ResponseDTO response = authService.doctorRegister(body);
 
-            newUser.setPassword(passwordEncoder.encode(body.password()));
-            newUser.setEmail(body.email());
-            newUser.setName(body.name());
-            newUser.setLastname(body.lastname());
-            newUser.setRole(Role.ROLE_DOCTOR);
-
-            this.userRepository.save(newUser);
-
-            String token = this.tokenService.generateToken(newUser);
-            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        if (response == null) {
+            return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(response);
     }
 }
---
+++ backend/src/main/java/com/project/medicalapp/services/AuthService.java
@@ -0,0 +1,83 @@
+package com.project.medicalapp.services;
+
+import com.project.medicalapp.dto.LoginRequestDTO;
+import com.project.medicalapp.dto.RegisterRequestDTO;
+import com.project.medicalapp.dto.ResponseDTO;
+import com.project.medicalapp.entities.Role;
+import com.project.medicalapp.entities.User;
+import com.project.medicalapp.infra.security.TokenService;
+import com.project.medicalapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @Transactional(readOnly = true)
+    public ResponseDTO login(LoginRequestDTO body) {
+        User user = this.userRepository.findByEmail(body.email())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(body.password(), user.getPassword())) {
+            return null;
+        }
+
+        String token = this.tokenService.generateToken(user);
+
+        return new ResponseDTO(user.getName(), token);
+    }
+
+    @Transactional
+    public ResponseDTO patientRegister(RegisterRequestDTO body) {
+        Optional<User> user = this.userRepository.findByEmail(body.email());
+
+        if (user.isPresent()) {
+            return null;
+        }
+
+        User newUser = new User();
+
+        newUser.setPassword(passwordEncoder.encode(body.password()));
+        newUser.setEmail(body.email());
+        newUser.setName(body.name());
+        newUser.setLastname(body.lastname());
+        newUser.setRole(Role.ROLE_PATIENT);
+
+        this.userRepository.save(newUser);
+
+        String token = this.tokenService.generateToken(newUser);
+
+        return new ResponseDTO(newUser.getName(), token);
+    }
+
+    @Transactional
+    public ResponseDTO doctorRegister(RegisterRequestDTO body) {
+        Optional<User> user = this.userRepository.findByEmail(body.email());
+
+        if (user.isPresent()) {
+            return null;
+        }
+
+        User newUser = new User();
+
+        newUser.setPassword(passwordEncoder.encode(body.password()));
+        newUser.setEmail(body.email());
+        newUser.setName(body.name());
+        newUser.setLastname(body.lastname());
+        newUser.setRole(Role.ROLE_DOCTOR);
+
+        this.userRepository.save(newUser);
+
+        String token = this.tokenService.generateToken(newUser);
+
+        return new ResponseDTO(newUser.getName(), token);
+    }
+}
