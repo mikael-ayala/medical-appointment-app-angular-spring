d56bc7bf009b4fafb7bc90381e6e0adcb52216ff
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-06-23 11:03:39 -0300

    fix: add cors package

--- backend/src/main/java/com/project/medicalapp/infra/cors/CorsConfig.java
+++ backend/src/main/java/com/project/medicalapp/infra/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.project.medicalapp.infra.security;
+package com.project.medicalapp.infra.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
